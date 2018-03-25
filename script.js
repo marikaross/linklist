3c8fb8928a60afaf7707e011e6a61af0c9a9e912
Marika Ross <marika.ross@gmail.com>
2018-03-25 12:02:05 -0600

    Create variables link html

---
+++ script.js
@@ -0,0 +1,6 @@
+var webTitle = document.querySelector('#web-title');
+var webUrl = document.querySelector('#web-url');
+var enterBtn = document.querySelector('.enter-button');
+var readBtn = document.querySelector('.read-button');
+var deleteBtn = document.querySelector('.delete-button');
+
