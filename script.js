005102441a838ff0565b9a92052ce971b70cd136
Marika Ross <marika.ross@gmail.com>
2018-03-28 12:49:38 -0600

    Fix button functions, update red toggle function

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-var inputTitle = $('.input-title');
-var inputUrl = $('.input-url');
-var enterBtn = $('.enter-button');
-var readBtn = $('.read-button');
-var deleteBtn = $('.delete-button');
-var rightSide = $('.right-side');
+var $inputTitle = $('.input-title');
+var $inputUrl = $('.input-url');
+var $enterBtn = $('.enter-button');
+var $readBtn = $('.read-button');
+var $deleteBtn = $('.delete-button');
+var $rightSide = $('.right-side');
+var $bookmarkArea = $('.total-counter');
 // var outputTitle = $('output-title');
 // var outputUrl = $('.output-url');
 
-enterBtn.on('click', newBookmark);
+$enterBtn.on('click', newBookmark);
 
 function newBookmark() {
-  rightSide.append(bookmarkContent);
+  $rightSide.append(bookmarkContent);
 }
 
 function bookmarkContent() {
-  var newTitle = inputTitle.val();
-  inputTitle.val('')
-  var newUrl = inputUrl.val();
-  inputUrl.val('')
-  
+  var newTitle = $inputTitle.val();
+  $inputTitle.val('')
+  var newUrl = $inputUrl.val();
+  $inputUrl.val('')
+  $enterBtn.prop('disabled', true);
   return (`<article class="bookmark">
             <h2 class="output-title">${newTitle}</h2>
             <hr>
@@ -30,17 +31,40 @@ function bookmarkContent() {
 };
 
 
-rightSide.on('click', '.read-button', function (event){
-$(event.currentTarget).toggleClass('color-red read-bookmark');
+$rightSide.on('click', '.read-button', function (event){ 
+  console.log($(this).closest('article'))
+$(this).closest('article').toggleClass('read-bookmark');
 });
 
-rightSide.on('click', '.delete-button', function (event){
+$rightSide.on('click', '.delete-button', function (event){
   $(this).closest('article').remove();
 });
 
-inputUrl.on('keydown',function() {
-  if(inputUrl.val() && inputTitle.val()) {
-    enterBtn.prop('disabled', false);
+$inputUrl.on('keydown',function() {
+  if($inputUrl.val() && $inputTitle.val()) {
+    $enterBtn.prop('disabled', false);
   }
 });
+
+
+
+// on enter bookmark counter ++ 
+// on delete bookmark counter --
+
+
+  // enterBtn.on('click', function() {
+  // var bookmarkCounter = 0;
+  //   bookmarkCounter ++
+  //   $('.total-counter').text('Bookmark count: ' +bookmarkCounter)
+  // });
+
+// bookmarkCounter = 0
+// bookmarkCounter ++
+// bookmarkArea.text('Bookmark count: ' +bookmarkCounter);
+
+
+// if ('.color-red' = true) {
+//   readcounter ++ && unreadcounter --
+// } else { 
+//   readcounter -- && unreadcounter ++}
 
